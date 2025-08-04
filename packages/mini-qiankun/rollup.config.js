fd845e404d3d48fdd24d5f103f6d20f7775f84d4
xuan <359499848@qq.com>
2025-08-04 23:40:56 +0800

    update build

--- packages/mini-qiankun/rollup.config.js
+++ packages/mini-qiankun/rollup.config.js
@@ -1,11 +1,10 @@
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import typescript from '@rollup/plugin-typescript';
-import dts from 'rollup-plugin-dts';
-
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const typescript = require('@rollup/plugin-typescript');
+const { dts } = require('rollup-plugin-dts');
 const packageJson = require('./package.json');
 
-export default [
+module.exports = [
   {
     input: 'src/index.ts',
     output: [
@@ -32,10 +31,7 @@ export default [
   },
   {
     input: 'src/index.ts',
-    output: [{
-      file: packageJson.types,
-      format: 'esm',
-    }],
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts()],
-  },
+  }
 ];
